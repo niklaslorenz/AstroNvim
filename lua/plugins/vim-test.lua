9035a1e3f0fbd56c488508ac88de33514750d7bb
Niklas Lorenz <niklaslorenz1999@gmail.com>
2025-01-13 16:57:19 +0100

    Fixed repository name for vim-test (What the fuck did you build be, ChatGPT?)

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,5 +1,5 @@
 return {
-  "jankoatwork/vim-test", -- vim-test plugin
+  "vim-test/vim-test", -- vim-test plugin
   ft = "python", -- Only load for Python files
   event = "VeryLazy", -- Lazy load when required
   config = function()
