b15d54f0c26e0368ed399ce76bc5941ab80a102d
Niklas Lorenz <niklaslorenz1999@gmail.com>
2025-05-04 17:52:33 +0200

    Added opts for code assist

--- lua/plugins/code-assist.lua
+++ lua/plugins/code-assist.lua
@@ -1,6 +1,14 @@
 return {
   "niklaslorenz/code-assist.nvim",
-  config = function() require("code-assist") end,
+  --- @type CodeAssistOptions
+  opts = {
+    model = "gpt-4o-mini",
+    system_message = "You are a helpful programming assistant.",
+    relative_chat_width = 0.4,
+    relative_chat_height = 0.4,
+    relative_chat_input_width = 0.4,
+    relative_chat_input_height = 0.4,
+  },
   lazy = false,
   keys = {},
   branch = "dev",
